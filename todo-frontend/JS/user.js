68203eac58c94e551331c01295c13252f80f01f0
KJH <kjhkmn80@gmail.com>
2026-05-13 20:49:36 +0900

    user.js(유저테이블 렌더링/유저 등록,수정,삭제)

---
+++ todo-frontend/JS/user.js
@@ -0,0 +1,83 @@
+// 더미 유저
+let users = [
+  { id: 1, name: "홍길동" },
+  { id: 2, name: "김철수" },
+];
+let nextId = users.length + 1;
+
+// 유저 테이블 렌더링
+function renderUsers() {
+  const tbody = document.querySelector("tbody");
+  tbody.innerHTML = "";
+
+  users.forEach((user) => {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+    <td>${user.id}</td>
+    <td>${user.name}</td>
+    <td><button class="btn-icon edit">수정</button></td>
+    <td><button class="btn-icon delete">삭제</button></td>`;
+
+    // 유저 삭제
+    tr.querySelector(".btn-icon.delete").addEventListener("click", (e) => {
+      users = users.filter((us) => us.id !== user.id);
+
+      renderUsers();
+    });
+
+    // 유저 수정
+    tr.querySelector(".btn-icon.edit").addEventListener("click", (e) => {
+      const tdName = tr.querySelectorAll("td")[1];
+      const currentName = tdName.textContent;
+      const editBtn = tr.querySelector(".btn-icon.edit");
+
+      if (editBtn.textContent === "저장") {
+        const newName = tdName.querySelector("input").value.trim();
+        if (!newName) return;
+        user.name = newName;
+        editBtn.textContent = "수정";
+        renderUsers();
+        return;
+      }
+
+      tdName.innerHTML = `<input type='text' value='${currentName}'>`;
+
+      const editInput = tdName.querySelector("input");
+      editInput.focus();
+      editBtn.textContent = "저장";
+
+      editInput.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") editBtn.click();
+      });
+    });
+
+    tbody.appendChild(tr);
+  });
+}
+renderUsers();
+
+// 유저 등록 - 버튼 클릭
+document.querySelector(".btn-icon.add").addEventListener("click", (e) => {
+  const inputText = document.querySelector("input");
+  const inputName = inputText.value.trim();
+
+  if (!inputName) {
+    alert("이름을 입력해주세요!");
+    inputText.focus();
+    return;
+  }
+
+  users.push({ id: nextId++, name: inputName });
+
+  inputText.value = "";
+
+  renderUsers();
+});
+
+// 유저 등록 - 입력 엔터
+document.querySelector("input").addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    document.querySelector(".btn-icon.add").click();
+  }
+});
